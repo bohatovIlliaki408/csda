8334c93893c6b3ed380a6b3a710c0c8f18dfd912
bohatovIlliaki408 <gratabis2@gmail.com>
2026-04-10 22:37:24 +0200

    Update checker.py 7 finale

--- checker.py
+++ checker.py
@@ -299,7 +299,7 @@ def main():
                 
                 if len(git_user) > 1 and len(repo_name) > 1:
                     status = check_repo(git_user, repo_name, student_name, group)
-                    logging.debug(f"{git_user}/{repo_name} -> {status}")
+                    logging.info(f"STATUS: {git_user}/{repo_name} -> {status}")
                     logging.info("")
                 else:
                     status = "EMPTY"
